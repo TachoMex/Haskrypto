a23fbd47d4f0539ef34d0e3765918aa146d8afe9
Gilberto Vargas <tachoguitar@gmail.com>
2016-09-22 23:35:23 -0500

    Implement ElGamal algorithm

---
+++ src/Haskrypto/Conversions.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Haskrypto.Conversions
+where
+  import qualified Data.ByteString.Lazy.Char8 as BL
+  import Data.Char
+  import GHC.Int
+  import Numeric (showIntAtBase, showHex, readHex)
+
+  hex n = showHex n ""
+
+  password_generate :: GHC.Int.Int64 -> BL.ByteString -> Integer
+  password_generate bytes message = unbincode aux_string 1 0
+    where
+      fixed_message = BL.take bytes $ BL.cycle message
+      aux_string = reverse $ BL.unpack fixed_message
+
+  unbincode :: String -> Integer -> Integer -> Integer
+  unbincode [] _ acum = acum
+  unbincode (c:rest) weight acum = unbincode rest weight' acum'
+    where
+      weight' =  weight * 256
+      acum' = acum + (fromIntegral $ ord c) * weight
+
+  bincode :: Integer -> String -> String
+  bincode 0 str = reverse str
+  bincode x str = bincode remainder (c:str)
+    where
+      (remainder, current_char) = divMod x 256
+      c = chr $ fromInteger current_char
