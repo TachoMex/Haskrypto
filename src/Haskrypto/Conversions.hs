4619056ed60e24a709ccd15281553527a1d1a3dc
Gilberto Vargas <tachoguitar@gmail.com>
2016-10-02 23:26:40 -0500

    Change function to internally provide extra params

--- src/Haskrypto/Conversions.hs
+++ src/Haskrypto/Conversions.hs
@@ -9,21 +9,25 @@ where
   hex n = showHex n ""
 
   password_generate :: GHC.Int.Int64 -> BL.ByteString -> Integer
-  password_generate bytes message = unbincode aux_string 1 0
+  password_generate bytes message = unbincode aux_string
     where
       fixed_message = BL.take bytes $ BL.cycle message
       aux_string = reverse $ BL.unpack fixed_message
 
-  unbincode :: String -> Integer -> Integer -> Integer
-  unbincode [] _ acum = acum
-  unbincode (c:rest) weight acum = unbincode rest weight' acum'
+  unbincode s = unbincode' s 1 0
+
+  unbincode' :: String -> Integer -> Integer -> Integer
+  unbincode' [] _ acum = acum
+  unbincode' (c:rest) weight acum = unbincode' rest weight' acum'
     where
       weight' =  weight * 256
       acum' = acum + (fromIntegral $ ord c) * weight
 
-  bincode :: Integer -> String -> String
-  bincode 0 str = reverse str
-  bincode x str = bincode remainder (c:str)
+  bincode i = bincode' i []
+
+  bincode' :: Integer -> String -> String
+  bincode' 0 str = reverse str
+  bincode' x str = bincode' remainder (c:str)
     where
       (remainder, current_char) = divMod x 256
       c = chr $ fromInteger current_char
